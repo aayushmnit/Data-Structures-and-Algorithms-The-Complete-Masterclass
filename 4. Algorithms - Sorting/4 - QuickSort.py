d3db16af925b20d37bcb46289d5f22a16288831f
aayushmnit <aayushmnit@gmail.com>
2021-10-03 13:21:08 -0700

    Adding Quick sort

--- 4. Algorithms - Sorting/4 - QuickSort.py
+++ 4. Algorithms - Sorting/4 - QuickSort.py
@@ -1,5 +1,5 @@
 def quickSort(my_array):
-    qshelper(my_array, 0, len(my_array) - 1)
+    qshelper(my_array, 0, len(my_array) - 1) ## Array, Starting value and ending value
     return my_array
 
 def qshelper(my_array, start, end):
@@ -11,17 +11,22 @@ def qshelper(my_array, start, end):
     right = end
 
     while right >= left:
-        
-        if my_array[left] > my_array[pivot] and my_array[right] < my_array[pivot]:
+        # Condition : 1    
+        if (my_array[left] > my_array[pivot]) and (my_array[right] < my_array[pivot]):
             my_array[left], my_array[right] = my_array[right], my_array[left]
         
-        if my_array[left] <= my_array[pivot]:
+        # Condition : 2
+        if my_array[left] <= my_array[pivot]: 
             left += 1
-        
-        if my_array[right] >= my_array[pivot]:
+
+        # Condition : 3        
+        if my_array[right] >= my_array[pivot]: 
             right -= 1
 
     my_array[pivot], my_array[right] = my_array[right], my_array[pivot]
 
-    qshelper(my_array, start, right - 1)
-    qshelper(my_array, right + 1, end)
+    qshelper(my_array, start, right-1)
+    qshelper(my_array, right+1, end)
+
+arr = [29, 10, 14, 27, 14]
+print(f'final:{quickSort(arr)}')
