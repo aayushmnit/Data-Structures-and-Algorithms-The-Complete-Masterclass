723f83508478f9323cd6c26ade8c11b176ba3889
aayushmnit <aayushmnit@gmail.com>
2021-10-04 16:04:29 -0700

    Implementing binary tree

--- 5. Algorithms - Tree Traversal/1 - BinaryTree-Implementation.py
+++ 5. Algorithms - Tree Traversal/1 - BinaryTree-Implementation.py
@@ -10,12 +10,12 @@ class BinaryTree():
         self.root = Node(value)
 
 
-tree = BinaryTree(3)
+tree = BinaryTree(3) ## Root
 
-tree.root.left = Node(4)
-tree.root.left.left = Node(6)
-tree.root.left.right = Node(7)
+tree.root.left = Node(4) ## Root LEFT
+tree.root.left.left = Node(6) ## Root Left left
+tree.root.left.right = Node(7) ## Root Right right
 
-tree.root.right = Node(5)
-tree.root.right.left = Node(8)
-tree.root.right.right = Node(9)
+tree.root.right = Node(5) ## Root Right
+tree.root.right.left = Node(8) ## Root Right left
+tree.root.right.right = Node(9) ## Root Right right
